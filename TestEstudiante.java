9ebcfcbcf6bd5d7b4c74847cb58f15465b60d670
Alex Calderón <acalderongdaw1@gmail.com>
2020-10-06 13:10:42 +0200

    Prueba ;)

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -20,6 +20,8 @@ public class TestEstudiante
         e3.printEstudiante();
         Estudiante e4 = new Estudiante("Roberto", 21);
         e4.printEstudiante();
+        Estudiante e5 = new Estudiante("Kike", 68);
+        e5.printEstudiante();
 
     }
 }
